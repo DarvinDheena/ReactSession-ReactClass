e93115c1144bb408280f11dc51d241d5ca8d6702
DarvinDheena <darvindheena98@gmail.com>
2023-09-27 13:24:09 +0530

    adding single component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 const App = ()=>{
   let time = new Date();
 
+  let a=10;
+  let b = 20;
+  let c = a+b ;
+
   return(
-    <div><p>Hello World!, now time is { time.toString() }</p></div>
+    <div><p>Hello World!, now time is { time.toString() } </p>
+    <p>{ c }</p>
+    </div>
+   
   )
   
 }
