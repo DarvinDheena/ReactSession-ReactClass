9189ec3642a9f47477c452820ae5e973a9157d2f
DarvinDheena <darvindheena98@gmail.com>
2023-09-28 11:23:46 +0530

    adding array of objects in a react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-function App() {
+function App( props ) {
+  console.log(props)
+  const { notes } = props
   return (
-    <div>App</div>
+    <div>
+      <p> {notes[0].content }</p>
+      <ul>
+        <li> {
+        notes[0].content }</li>
+        <li>{ notes[1].content }</li>
+        <li>{ notes[2].content }</li>
+        <li> { notes[3].content}</li>
+        <li>{ notes[4].content }</li>
+
+      </ul>
+    </div>
+    
   )
 }
 
