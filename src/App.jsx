b2610e74372cc8fb541f035c5bac94b3fa177169
DarvinDheena <darvindheena98@gmail.com>
2023-09-30 11:00:54 +0530

    adaing states with multiple buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
 import React, { Component } from 'react'
 
-export default class extends Component {
+export default class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            count : 0,
+            count :0,
         }
     }
+increment = ()=>{
+    this.setState((prevState) => ({
+        count : prevState.count + 1,
+    }))
+}
+decrement = () => {
+if ( this.state.count > 0) {
+    this.setState((prevState) => ({
+        count :prevState.count - 1, 
+    }))
+}
+}
 
-    increment = ()=>{
-        this.setState({
-            count : this.state.count + 1 ,
-        })
-    }
+reset = () => {
+    this.setState((prevState) => ({
+        count :0 ,
+    })) 
+}
 
   render() {
     return (
       <div>
-        <p>Count : { this.state.count }</p>
+        <p>count : { this.state.count}</p>
         <button onClick={ this.increment}>Increment</button>
+        <button onClick={ this.decrement}>Decrement</button>
+        <button onClick={ this.reset}>Reset</button>
       </div>
     )
   }
 }
-
