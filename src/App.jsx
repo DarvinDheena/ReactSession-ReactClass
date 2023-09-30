744f4e83266b80af4aedf52f7ad66f4aa0e3fc57
DarvinDheena <darvindheena98@gmail.com>
2023-09-30 17:11:06 +0530

    adding zero button

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import React, { useState } from 'react'
 function App() {
     const [ counter , setCounter] = useState(0);
 
-    const handleClick = ()=>{
+    const handlePlusClick = ()=>{
         setCounter(counter + 1);
+    }
+    const handleZeroClick = ()=>{
+        setCounter(0);
     }
   return (
     <div>
         <div>{ counter }</div>
-        <button onClick={ handleClick }> Increment </button>
+        <button onClick={ handlePlusClick }> Increment </button>
+        <button onClick={ handleZeroClick }> Zero </button>
+
     </div>
   )
 }
