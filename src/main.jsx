ce70c3efcd202cafea64c60bc26c19ca1c5b5243
DarvinDheena <darvindheena98@gmail.com>
2023-09-30 07:15:47 +0530

    adding class components with syntex

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import'./App.css'
 
 const notes = [
     {
@@ -30,4 +31,4 @@ const notes = [
     }
 ];
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App notes = { notes } />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
