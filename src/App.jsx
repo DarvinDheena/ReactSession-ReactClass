b70409681f285f04504a6d94066a38d703469e26
DarvinDheena <darvindheena98@gmail.com>
2023-09-30 11:49:46 +0530

    adding bootstrap in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            count :0,
-        }
-    }
-increment = ()=>{
-    this.setState((prevState) => ({
-        count : prevState.count + 1,
-    }))
-}
-decrement = () => {
-if ( this.state.count > 0) {
-    this.setState((prevState) => ({
-        count :prevState.count - 1, 
-    }))
-}
+function App() {
+  return (
+    <div>
+        <h1 className='mt-10'>hello world </h1>
+        <p className='p-5 mt-10'>Darviin</p>
+    </div>
+  )
 }
 
-reset = () => {
-    this.setState((prevState) => ({
-        count :0 ,
-    })) 
-}
-
-  render() {
-    return (
-      <div>
-        <p>count : { this.state.count}</p>
-        <button onClick={ this.increment}>Increment</button>
-        <button onClick={ this.decrement}>Decrement</button>
-        <button onClick={ this.reset}>Reset</button>
-      </div>
-    )
-  }
-}
+export default App
