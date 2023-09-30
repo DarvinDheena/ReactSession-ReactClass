7ccc32ee305a9ff83a5bf9df1b8902f6f86663c5
DarvinDheena <darvindheena98@gmail.com>
2023-09-30 12:20:55 +0530

    complete a class task with 3 buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-        <h1 className='mt-10'>hello world </h1>
-        <p className='p-5 mt-10'>Darviin</p>
-    </div>
-  )
+export default class extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            good:0,
+            neutral:0,
+            bad:0,
+        }
+    }
+
+    handleGoodClick = ()=>{
+        this.setState({
+            ...this.state,
+            good:this.state.good + 1,
+        })
+    }
+    handleNeutralClick = ()=>{
+        this.setState({
+            ...this.state,
+            neutral:this.state.neutral + 1,
+        })
+    }
+    handleBadClick = ()=>{
+        this.setState({
+            ...this.state,
+            bad:this.state.bad + 1,
+        })
+    }
+
+  render() {
+    return (
+      <div>
+         <h1>Give FeedBack</h1>
+        <button onClick={ this.handleGoodClick }>good </button>
+        <button onClick={ this.handleNeutralClick }>neutral</button>
+        <button onClick={ this.handleBadClick }>bad</button>
+
+        <h1>Statistics</h1>
+        <p>good : { this.state.good }</p>
+        <p>neutral : { this.state.neutral}</p>
+        <p>bad : { this.state.bad}</p>
+      </div>
+    )
+  }
 }
-
-export default App
