ab9de13e58f5362fbfbe44162554f446a78f48be
DarvinDheena <darvindheena98@gmail.com>
2023-10-14 17:08:21 +0530

    adding useref by simple example

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,23 @@
-import React, { useState } from 'react'
-import Note from './Components/Note';
-function App(props) {
+import React from 'react'
+import { useRef } from 'react'
 
-  const [notes,setNotes] = useState(props.notes);
-  const [newNote,setNewNote] = useState('');
 
-  function addNote(event){
-    event.preventdefault();
-    console.log(newNote)
-    const noteObject = {
-      id : notes.length +1 ,
-      content : newNote,
-      important : true 
-    }
-    setNotes(notes.concat(noteObject));
-    console.log(notes);
-    setNewNote('');
-  };
 
-  const handleNoteChange = (event)=>{
-    setNewNote(event.target.value);
-    console.log(newNote)
+function App() {
+
+  const inputRef = useRef(null);
+
+  const handleClick = () =>{
+    inputRef.current.focus();
   }
 
   return (
     <div>
-      <h1>Basic FORM </h1>
-      <div>
-        <ul>
-            {
-                notes.map (note=>
-                  <Note key={note.id} note={ note }/>
-                )
-            }
-        </ul>
-      </div>
-      <form onSubmit={addNote}>
-        <input 
-        value ={ newNote }
-        placeholder='add New note...'
-        onChange={handleNoteChange}
-        
-        />
-        <button type='submit'>Save</button>
-      </form>
-
+      <input 
+      type='text'
+      ref={inputRef}
+      />
+      <button onClick={handleClick}>focus input</button>
     </div>
   )
 }
