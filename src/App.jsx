8ca3417d25eb10e6233e520128c258a929ccd3b0
DarvinDheena <darvindheena98@gmail.com>
2023-10-20 15:40:34 +0530

    useReducer hook example 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ const initialState = {
 function reducer(state,action){
   switch(action.type){
     case 'toggle':return { isActive: !state.isActive }
+    case 'toactive' : return { isActive : true }
+    case 'toinactive' : return { isActive : false }
   }
 }
 
@@ -17,6 +19,8 @@ function App() {
       <h1>toggle Button</h1>
       <p>Activate profile : { state.isActive ? 'yes' : ' no'  }</p>
       <button onClick={ ()=> dispatch({type:'toggle'})}>toggle profile</button>
+      <button onClick={()=>dispatch({type : 'toactive'})}>toactive</button>
+      <button onClick={()=>dispatch({type : 'toinactive'})}>toinactive</button>
     </div>
   )
 }
